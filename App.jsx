2651c9a6d4f466d22d78b4bbe13b8ec1d7875656
HemasriMurugakani <hemaleena1102@gmail.com>
2024-09-25 16:52:02 +0530

    Basic Setup Done

--- App.jsx
+++ App.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignInScreen from './src/screens/actions/authentication/signIn';
+import SignUpScreen from './src/screens/actions/authentication/signUp';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
 

78aae3fb05cf10fe8af83ac1676f32c790eb8c93
Thulasimani5 <thulasimanid.cb23@bitsathy.ac.in>
2024-09-26 10:44:05 +0530

    Changes from team-a

--- App.jsx
+++ App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/Home'
-
+import Product_Detail from './src/screens/Product_Detail'
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Product_Detail">
+        <Stack.Screen name="Product_Detail" component={Product_Detail} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
